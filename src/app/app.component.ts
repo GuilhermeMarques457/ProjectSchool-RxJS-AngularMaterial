af9a27beba5dc0829a3215c04428463937df5c7b
Guilherme Marques <guilherme.marques.santos457@gmail.com>
2023-12-04 21:00:21 -0300

    add-inital-files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
-  title = 'sla';
+  public isSmallScreen = false;
+  title = 'ProjectSchool';
+
+  get sidenavMode() {
+    return this.isSmallScreen ? 'over' : 'side';
+  }
 }
